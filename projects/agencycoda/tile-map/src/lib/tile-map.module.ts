4edbdf1d200652bae80b5c82f2f6502d0a597c5d
Matias Camiletti <matias.camiletti@gmail.com>
2022-06-21 12:41:09 -0300

    Install angular material.

--- projects/agencycoda/tile-map/src/lib/tile-map.module.ts
+++ projects/agencycoda/tile-map/src/lib/tile-map.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
-import { TileMapComponent } from './tile-map.component';
+
 import { TileMapCanvaComponent } from './components/tile-map-canva/tile-map-canva.component';
 
 
 
 @NgModule({
   declarations: [
-    TileMapComponent,
     TileMapCanvaComponent
   ],
   imports: [
   ],
   exports: [
-    TileMapComponent,
     TileMapCanvaComponent
   ]
 })
--- projects/agencycoda/tile-map/src/public-api.ts
+++ projects/agencycoda/tile-map/src/public-api.ts
@@ -2,6 +2,11 @@
  * Public API Surface of tile-map
  */
 
+/*
+ * Components
+ */
+export * from './lib/components/tile-map-canva/tile-map-canva.component';
+
+
 export * from './lib/tile-map.service';
-export * from './lib/tile-map.component';
 export * from './lib/tile-map.module';
